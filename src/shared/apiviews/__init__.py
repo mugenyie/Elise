01a3c8a4a54f0319b568028303650908e3093b79
Emmanuel Columbus Mugenyi <ecmugenyi@gmail.com>
2019-05-04 04:39:35 +0300

    Edit Account Tests

--- src/shared/apiviews/__init__.py
+++ src/shared/apiviews/__init__.py
@@ -1,11 +1,7 @@
-from flask import json, Response
+from flask import jsonify
 
 def custom_response(data, status_code):
     """
     Custom Response Function
     """
-    return Response(
-        mimetype="application/json",
-        response=json.dumps(data),
-        status=status_code
-    )
+    return jsonify(data), status_code
--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,4 +1,4 @@
-import json
+from flask import json, jsonify
 from .BaseTestCase import BaseTestCase
 
 class accountsTest(BaseTestCase):
